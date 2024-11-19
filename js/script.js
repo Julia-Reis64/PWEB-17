4b8adc810111cf20435390ba32ba5c72885d9e45
Julia <julyreis.correa@gmail.com>
2024-11-18 22:20:27 -0300

    JS WOOOH

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+PROCEDIMENTO
+Descrição: Exibe uma mensagem
+Nome: mostraAlerta
+Parâmetro: void
+Retorno: void
+void -> significa que não retorna valor */
+
+/* function mostraAlerta() {
+    alert("Não é que isso funciona mesmo??");
+}
+
+function mostraAlerta1(msg) {
+    alert (`Boa noite, ${msg}`)
+}
+
+function mensagem_com_parametro_return(msg1) {
+    return("Você digitou: " +msg1)
+} */
+
+function soma(n1,n2) {
+    var soma;
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    soma= n1+n2; 
+    return("resultado: "+soma)
+
+}
